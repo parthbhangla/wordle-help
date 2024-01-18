0b66e4e2d82c5ce2af2772fab431e63b18329faa
parthbhangla <parthbhangla987@gmail.com>
2024-01-18 00:59:07 -0500

    added filter to only have alpha words

--- sorter.py
+++ sorter.py
@@ -2,7 +2,7 @@ input = open('words.txt', 'r')
 output = open('five_letter_words.txt', 'w')
 for word in input:
     word = word.strip().lower()
-    if len(word) == 5:
+    if len(word) == 5 and word.isalpha():
         output.write(word+"\n")
     else:
         continue
