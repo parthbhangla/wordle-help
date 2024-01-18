dc66589026607ce5f75ac3f1f02e7e699234bdd8
parthbhangla <parthbhangla987@gmail.com>
2024-01-18 17:09:24 -0500

    made code clearer

--- main.py
+++ main.py
@@ -16,27 +16,23 @@ except FileNotFoundError:
     exit()
 
 while letter != '':
+
     word_list.clear()
 
     letter = input("What letter do you know? ")
     colour = input("What colour is the letter (b, y, g)? ")
 
     if colour == 'g' or colour == 'y':
-        position = int(input("What position is the letter in (1-5)? "))
-        position = position - 1
+        position = int(input("What position is the letter in (1-5)? ")) - 1
     
     for word in possible_words:
-        if colour == 'g':
-            if word[position] == letter:
-                word_list.append(word)
+        if colour == 'g' and word[position] == letter:
+             word_list.append(word)
 
-        elif colour == 'y':
-            if letter in word:
-                if word[position] != letter:
-                    word_list.append(word)
+        elif colour == 'y' and letter in word and word[position] != letter:
+            word_list.append(word)
 
-        elif colour == 'b':
-            if letter not in word:
+        elif colour == 'b' and letter not in word:
                 word_list.append(word)
 
     possible_words = word_list.copy()
