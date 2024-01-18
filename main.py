8213aff9154471717cf9b25f2c97da62234e423b
parthbhangla <parthbhangla987@gmail.com>
2024-01-18 16:49:01 -0500

    completed basic elimination

---
+++ main.py
@@ -0,0 +1,45 @@
+# Variables for the program
+possible_words = []
+letter = 'a'
+position = 1
+word_list = []
+
+print("Wordle Helper By Parth Bhangla \n")
+print("This program uses basic elimination on all the acceptable answers of wordle to solve it.")
+print("To use this, you need to make the first guess and then feed the information you get into to program and take another guess from the remaining words list and continue doing so until you find the answer. \n")
+
+try:
+    with open("answers.txt", "r") as f:
+        possible_words = f.readlines()
+except FileNotFoundError:
+    print("Error: 'answers.txt' not found.")
+    exit()
+
+while letter != '':
+    word_list.clear()
+
+    letter = input("What letter do you know? ")
+    colour = input("What colour is the letter (b, y, g)? ")
+
+    if colour == 'g' or colour == 'y':
+        position = int(input("What position is the letter in (1-5)? "))
+        position = position - 1
+    
+    for word in possible_words:
+        if colour == 'g':
+            if word[position] == letter:
+                word_list.append(word)
+
+        elif colour == 'y':
+            if letter in word:
+                if word[position] != letter:
+                    word_list.append(word)
+
+        elif colour == 'b':
+            if letter not in word:
+                word_list.append(word)
+
+    possible_words = word_list.copy()
+    print("Remaining Possible Words: \n")
+    print(*word_list, end='\n')
+        
